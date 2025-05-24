753995857b4d10212446dca1a537d7a7a325e887
yooodleee <accia25@naver.com>
2025-05-25 01:49:09 +0900

    prepare

---
+++ simple-springboot-proj/src/main/java/com/example/simplespringbootproj/SimpleSpringbootProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplespringbootproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringbootProjApplication.class, args);
+    }
+
+}
---
+++ simple-springboot-proj/src/test/java/com/example/simplespringbootproj/SimpleSpringbootProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplespringbootproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringbootProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
